4dc6f43e8cfd71c4877aacdaa88b41443c1b0299
Trictonicmp <code_shadow@hotmail.com>
2022-05-02 16:31:55 -0500

    Whole application

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+/* ------------------ CSS ------------------ */
+import '../css/main.css';
+
+/* ------------- Custom modules ------------ */
+import DataHandler from './DataHandler.js';
+
+const dataHandler = new DataHandler();
+dataHandler.init();
